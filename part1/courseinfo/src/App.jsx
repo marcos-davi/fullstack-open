c23392ee2e5d173547ebb4b00b8aa0c4efd4b9f7
Marcos Davi <mdmalungo@gmail.com>
2024-05-07 17:54:48 +0200

    courseinfo 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,71 +1,70 @@
-const Header = (props) => {  
+const Header = (props) => {
   return (
     <div>
       <h1>{props.course}</h1>
     </div>
-  )
-}
+  );
+};
 
 const Part = (props) => {
-  return (   
+  const part = [];
+  return (
     <p>
-  {props.part} {props.exercises}  
-  </p>
- 
-)
+      {props.part} {props.exercises}
+    </p>
+  );
+};
 
-}
-
-const Contents = (props) => {  
-  return (   
-        <div>
-      <Part part = {props.part1} exercises = {props.exercises1}/> 
-      <Part part = {props.part2} exercises = {props.exercises2}/> 
-      <Part part = {props.part3} exercises = {props.exercises3}/>      
-      </div>
-     
-  )
-}
+const Contents = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  );
+};
 
 const Total = (props) => {
-  return(
-    <p>Number of exercises {props.exercises}</p> 
-  )
-}
+  return <p>Number of exercises {props.exercises}</p>;
+};
 
-const App = () => { 
-  
-  const course = 'Half Stack application development'
+const App = () => {
+  const course = "Half Stack application development";
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-}
-const part2 = {
-  name: 'Using props to pass data',
-  exercises: 7
-}
-
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  
-  
-  
-  
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
-      <Header course = {course} />
+      <Header course={course} />
 
-      <Contents part1 = {part1.name}  part2 = {part2.name} part3 = {part3.name} 
-      exercises1 = {part1.exercises} exercises2 = {part2.exercises} exercises3 = {part3.exercises}/>
-     
-      <Total exercises = {part1.exercises + part2.exercises + part3.exercises} />    
-      </div>
-  )
-}
+      <Contents
+        part1={parts[0].name}
+        part2={parts[1].name}
+        part3={parts[2].name}
+        exercises1={parts[0].exercises}
+        exercises2={parts[1].exercises}
+        exercises3={parts[2].exercises}
+      />
 
-export default App
+      <Total
+        exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      />
+    </div>
+  );
+};
+
+export default App;

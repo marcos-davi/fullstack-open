2b06a0d62d1634f291f830ab53b20c9f7e33e2e7
Marcos Davi <mdmalungo@gmail.com>
2024-05-11 19:51:28 +0200

    control de eventos

--- outro/src/App.jsx
+++ outro/src/App.jsx
@@ -1,17 +1,24 @@
 import { useState } from 'react'
 
 const App = () => {
-
   const [ counter, setCounter ] = useState(0)
 
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+
+      <button onClick={setToZero}>
+        zero
+      </button>
+    </div>
   )
 }
 

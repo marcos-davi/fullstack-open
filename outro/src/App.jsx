fe0e46c116b439290f2a6102ab02cc627505aab4
Marcos Davi <mdmalungo@gmail.com>
2024-05-13 00:36:27 +0200

    Los cambios en el estado provocan re-renderizado

--- outro/src/App.jsx
+++ outro/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  console.log("rendering with counter value", counter);
 
   const Display = (props) => {
     return <div>{props.counter}</div>;
@@ -10,14 +11,24 @@ const App = () => {
     return <button onClick={props.onClick}>{props.text}</button>;
   };
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const increaseByOne = () => {
+    console.log("increasing, value before", counter);
+    setCounter(counter + 1);
+  };
+
+  const decreaseByOne = () => {
+    console.log("decreasing, value before", counter);
+    setCounter(counter - 1);
+  };
+
+  const setToZero = () => {
+    console.log("resetting to zero, value before", counter);
+    setCounter(0);
+  };
 
   return (
     <div>
       <Display counter={counter} />
-
       <Button onClick={increaseByOne} text="plus" />
       <Button onClick={setToZero} text="zero" />
       <Button onClick={decreaseByOne} text="minus" />

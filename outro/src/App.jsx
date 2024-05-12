b64f8c670d351ecb2b203a900c77f911a98e99f0
Marcos Davi <mdmalungo@gmail.com>
2024-05-13 00:44:46 +0200

    Refactorización de los componentes

--- outro/src/App.jsx
+++ outro/src/App.jsx
@@ -2,29 +2,15 @@ import { useState } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
-  console.log("rendering with counter value", counter);
+  
+  const Display = ({ counter }) => <div>{counter}</div>
 
-  const Display = (props) => {
-    return <div>{props.counter}</div>;
-  };
-  const Button = (props) => {
-    return <button onClick={props.onClick}>{props.text}</button>;
-  };
+  const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-  const increaseByOne = () => {
-    console.log("increasing, value before", counter);
-    setCounter(counter + 1);
-  };
 
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter);
-    setCounter(counter - 1);
-  };
-
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
-  };
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
 
   return (
     <div>

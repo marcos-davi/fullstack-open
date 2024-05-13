e621d496c921ed72c9c24af87ce4fa81dfe979fa
Marcos Davi <mdmalungo@gmail.com>
2024-05-13 13:32:43 +0200

    Manejo de arrays

--- outro/src/App.jsx
+++ outro/src/App.jsx
@@ -1,32 +1,31 @@
 import { useState } from "react";
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
-  const handleLeftClick = () =>
-    setClicks({ ...clicks, left: clicks.left + 1 })
-  
-  const handleRightClick = () =>
-    setClicks({ ...clicks, right: clicks.right + 1 })
+  const [allClicks, setAll] = useState([])
 
-  const handleZeroClick = () => {
-    const newClicks = {
-      left: clicks.left = 0,
-      right: clicks.right = 0
-    }
-    setClicks(newClicks)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleZeroClick}>reset</button>
       <button onClick={handleRightClick}>right</button>
-      
-      {clicks.right}
+      {right}
+
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }

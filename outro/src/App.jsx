ac447c0f6fd34235a47b3a98f680726b8e462e9b
Marcos Davi <mdmalungo@gmail.com>
2024-05-13 13:21:02 +0200

    Estado complejo

--- outro/src/App.jsx
+++ outro/src/App.jsx
@@ -1,25 +1,33 @@
 import { useState } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: clicks.left + 1 })
   
-  const Display = ({ counter }) => <div>{counter}</div>
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 })
 
-  const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
-
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const handleZeroClick = () => {
+    const newClicks = {
+      left: clicks.left = 0,
+      right: clicks.right = 0
+    }
+    setClicks(newClicks)
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleZeroClick}>reset</button>
+      <button onClick={handleRightClick}>right</button>
+      
+      {clicks.right}
     </div>
-  );
-};
-
+  )
+}
 export default App;

088a3202745068f8c2f58abaeb1eb596bdef3114
Marcos Davi <mdmalungo@gmail.com>
2024-05-14 22:12:48 +0200

    No definir componentes dentro de los componentes

--- outro/src/App.jsx
+++ outro/src/App.jsx
@@ -1,30 +1,26 @@
 import { useState } from "react";
+// Este es lugar correcto para definir un componente
+const Display = (props) => <div>{props.value}</div>;
 
+// Este es lugar correcto para definir un componente
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
-  const [value, setValue] = useState(10)
-  
+  const [value, setValue] = useState(10);
 
   const setToValue = (newValue) => () => {
-    console.log('value now', newValue)  // imprime el nuevo valor en la consola
-    setValue(newValue)
-  }
-  const Button = (props) => (
-    <button onClick={props.handleClick}>
-      {props.text}
-    </button>
-  )
-  
-
-
+    console.log("value now", newValue); // imprime el nuevo valor en la consola
+    setValue(newValue);
+  };
   return (
     <div>
-      {value}
-     
+      <Display value={value} />
       <Button handleClick={setToValue(1000)} text="thousand" />
       <Button handleClick={setToValue(0)} text="reset" />
       <Button handleClick={setToValue(value + 1)} text="increment" />
     </div>
-  )
-}
+  );
+};
 export default App;

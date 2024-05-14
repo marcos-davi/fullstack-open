2fd3e6d967746d44ad1a1be39baeb315446e076d
Marcos Davi <mdmalungo@gmail.com>
2024-05-14 19:43:05 +0200

    Una función que devuelve una función

--- outro/src/App.jsx
+++ outro/src/App.jsx
@@ -1,56 +1,21 @@
 import { useState } from "react";
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  
+  const [value, setValue] = useState(10)
 
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-   
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // imprime el nuevo valor en la consola
+    setValue(newValue)
   }
 
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-   
-  }
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks ={allClicks} />
-       </div>
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
+    </div>
   )
 }
 export default App;

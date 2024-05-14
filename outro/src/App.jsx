b0cdd1ebfd444f0f65efa2d0ab7d87c01407fffe
Marcos Davi <mdmalungo@gmail.com>
2024-05-14 21:45:31 +0200

    Pasando controladores de eventos a componentes hijos

--- outro/src/App.jsx
+++ outro/src/App.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
 
+
 const App = () => {
   const [value, setValue] = useState(10)
+  
 
   const setToValue = (newValue) => () => {
     console.log('value now', newValue)  // imprime el nuevo valor en la consola
     setValue(newValue)
   }
+  const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+  
 
 
   return (
     <div>
       {value}
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+     
+      <Button handleClick={setToValue(1000)} text="thousand" />
+      <Button handleClick={setToValue(0)} text="reset" />
+      <Button handleClick={setToValue(value + 1)} text="increment" />
     </div>
   )
 }

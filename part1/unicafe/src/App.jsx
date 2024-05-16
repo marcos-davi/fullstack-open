88323b64a7d2a18a5f8b9aecab600c514d5b3b79
Marcos Davi <mdmalungo@gmail.com>
2024-05-16 14:07:39 +0200

    unicafe1.9 step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,37 +5,47 @@ const Head = (props) => <h1>{props.text}</h1>;
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 );
-const Display = (props) => (
-  <p>
+const Display = (props) => {
+//  if(props.value === 0 || props.value === "0%") {
+//     return(     
+//     <div>{"Vacio"}</div>  
+//     )
+//   }
+  return (
+    <p>
     {props.text} {props.value}
   </p>
-);
-
-const Statistics = (props) => (
-  <div>
-    <Head text={props.textHead1} />
-
-    <Button handleClick={props.handle1} text={props.textButton1} />
-    <Button handleClick={props.handle2} text={props.textButton2} />
-    <Button handleClick={props.handle3} text={props.textButton3} />
-
-    <Head text={props.textHead2} />
-
+  )
+};
+ 
+const Statistics = (props) => {
+  if (props.allClicks === 0){
+    return(
+      <div>
+        No feedback given
+      </div>
+    )  }
+    return(
+    <div>
     <Display text={props.textDisplay1} value={props.valueDisplay1} />
     <Display text={props.textDisplay2} value={props.valueDisplay2} />
     <Display text={props.textDisplay3} value={props.valueDisplay3} />
     <Display text={props.textDisplay4} value={props.valueDisplay4} />
     <Display text={props.textDisplay5} value={props.valueDisplay5} />
   </div>
-);
+  )
+  
+};
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allClicks, setAllClicks] = useState(0);
 
-  const setToStatisc = (value, set) => {
+  const setToStatisc = (value, set, allClicks, setAllClicks) => {
+    setAllClicks(allClicks + 1)
     set(value + 1);
   };
 
@@ -60,19 +70,21 @@ const App = () => {
 
   return (
     <div>
+
+      <Head text="Give Feedback" />
+
+      <Button handleClick={() => setToStatisc(good, setGood, allClicks, setAllClicks)} text="Good"/>
+      <Button handleClick={() => setToStatisc(neutral, setNeutral, allClicks, setAllClicks)} text="Neutral" />
+      <Button handleClick={() => setToStatisc(bad, setBad, allClicks, setAllClicks)} text="Bad" />
+
+      <Head text="Statiscs" />
+
       <Statistics
-        textHead1="Give Feedback"
-        handle1={() => setToStatisc(good, setGood)}
-        textButton1="Good"
+        allClicks = {allClicks}
         textDisplay1="Good: "
         valueDisplay1={good}
-        textHead2="Statiscs"
-        handle2={() => setToStatisc(neutral, setNeutral)}
-        textButton2="Neutral"
         textDisplay2="Neutral: "
         valueDisplay2={neutral}
-        handle3={() => setToStatisc(bad, setBad)}
-        textButton3="Bad"
         textDisplay3="Bad: "
         valueDisplay3={bad}
         textDisplay4="Average: "
